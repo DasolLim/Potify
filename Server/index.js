e0d1b11e95157720bb46711510611087312f26b3
Dasol Lim <dlim67@uwo.ca>
2024-11-19 15:58:42 -0500

    Creating connection to MySQL and logging in

--- Server/index.js
+++ Server/index.js
@@ -5,3 +5,21 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 app.use(cors());
+
+// FIRST cd to the DUMP folder
+// THEN TO RUN THE SCRIPT RUN: 'node db.js'
+
+// HOW TO USE:
+// do 'npm i mysql' to download dependencies
+// also do 'npm -y' to initialize package.json
+const connection = createConnection({
+    host: 'localhost',
+    user: 'root',
+    // CHANGE WITH YOUR MYSQL PASSWORD
+    // *** ALSO NEED THE DATABASE CREATED IN MYSQL WORKBENCH
+    password: '123!@#QWEasdzxc',
+    database: 'Potify',
+    port: 3306,
+});
+
+connection.connect();
